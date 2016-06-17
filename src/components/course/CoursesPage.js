1b39b66dca270383d22143e2a136d436bc5c6a64
matthew.french@gmail.com <matthew.french@gmail.com>
2016-06-17 15:38:14 +0100

    CoursesPage

---
+++ src/components/course/CoursesPage.js
@@ -0,0 +1,14 @@
+import React, {PropTypes} from 'react';
+
+class CoursePage extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Courses</h1>
+                <p>React, Redux and React Router in ES6 for ultra-responsive web apps.</p>
+            </div>
+        );
+    }
+}
+
+export default CoursePage;
