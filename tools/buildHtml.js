cd5adb0523e6f84bddf136c261e29f8ff8bfcdcb
matthew.french@gmail.com <matthew.french@gmail.com>
2016-09-23 15:43:35 +0100

    dist>public

--- tools/buildHtml.js
+++ tools/buildHtml.js
@@ -17,10 +17,10 @@ fs.readFile('src/index.html', 'utf8', (err, markup) => {
   // since a separate spreadsheet is only utilized for the production build, need to dynamically add this here.
   $('head').prepend('<link rel="stylesheet" href="styles.css">');
 
-  fs.writeFile('dist/index.html', $.html(), 'utf8', function (err) {
+  fs.writeFile('public/index.html', $.html(), 'utf8', function (err) {
     if (err) {
       return console.log(err);
     }
-    console.log('index.html written to /dist'.green);
+    console.log('index.html written to /public'.green);
   });
 });
